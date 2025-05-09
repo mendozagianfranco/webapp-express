24b5d2837f912f95abb66be04bc9deccf76e8516
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-05-09 15:56:11 +0200

    feat: add middleware errorsHandler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT;
 const moviesRouter = require('./routers/moviesRouter');
 const notFound = require('./middlewares/notFound');
+const errorsHandler = require('./middlewares/errorsHandler');
 
 app.use(express.static('public'));
 
@@ -13,6 +14,7 @@ app.get('/', (req, res) => {
 app.use('/movies', moviesRouter);
 
 app.use(notFound);
+app.use(errorsHandler);
 
 app.listen(port, () => {
     console.log('Server in ascolto sul port: ' + port);
